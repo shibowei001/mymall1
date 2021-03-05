eb21134628f7c9a86fb53b1b1800c5110cf33c9d
shibowei <675279526@qq.com>
2021-03-05 17:44:05 +0800

    20210.05

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id = "tab-bar">
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+ export default {
+   name: 'TabBar'
+ }
+</script>
+
+<style>
+  #tab-bar{
+    display: flex;
+    background-color: #f6f6f6;
+
+    position: fixed;
+    right: 0px;
+    left: 0px;
+    bottom: 0px;
+
+    box-shadow: 0px -1px 1px rgba(150,150,150,.08);
+  }
+  
+</style>
